6bbdf9fe7cca97dd31b1fa6493a1eeade3c8b810
WanneeSr <93991020+WanneeSr@users.noreply.github.com>
2025-11-12 07:46:42 +0000

    update

--- controllers/users.js
+++ controllers/users.js
@@ -5,8 +5,8 @@ const { query } = require('../config/db')
 const getUsers = async (request, response) => {
     try {
         const sql = 'SELECT * FROM users';
-        await query(sql);
-        response.status(200),send({})
+        const rows = await query(sql);
+        response.status(200),send(rows)
         
     } catch (error) {
         console.log('Error : ',error);
