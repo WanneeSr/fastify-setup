58ca8659e75a89a68d8d36e1d46560d6b6410f4c
WanneeSr <93991020+WanneeSr@users.noreply.github.com>
2025-11-12 07:13:14 +0000

    update

--- controllers/users.js
+++ controllers/users.js
@@ -1,1 +1,19 @@
+const { query } = require('../config/db')
 
+
+
+const getUsers = async (request, response) => {
+    try {
+        const sql = 'SELECT * FROM users';
+        await query(sql);
+        response.status(200),send({})
+        
+    } catch (error) {
+        console.log('Error : ',error);
+        response.status(500).send({ message: 'Internal Server Error'})
+    }
+}
+
+module.exports = {
+    getUsers
+}
--- server.js
+++ server.js
@@ -1,13 +1,49 @@
 const fastify = require('fastify')({ logger:true })
+const util = require("node:util");
+const { pipeline } = require("node:stream");
+const pump = util.promisify(pipeline);
+const path = require("path");
+const { log } = require('node:console');
+const { default: fastifyFormbody } = require('@fastify/formbody');
+const { default: fastifyWebsocket } = require('@fastify/websocket');
+const { Socket } = require('node:dgram');
+
+
+fastify.register(require('@fastify/formbody'));
+fastify.register(require("@fastify/multipart"));
+fastify.register(require('@fastify/jwt'), { secret: 'lookthorwow' });
+fastify.register(require('@fastify/cors'), {});
+fastify.register(require('@fastify/websocket'))
+fastify.register(async function (fastify) {
+  fastify.get('/websocket',{ websocket:true}, (socket, req)=>{
+    socket.on('message', message =>{
+      socket.send('ready from server')
+    })
+  })
+})
+
+fastify.register(require("@fastify/static"), {
+    root: path.join(__dirname, 'uploads'),
+    prefix: '/uploads/',
+});
+
+
+
+
+
+
+
 
 fastify.get('/', function handler (request, reply){
   reply.send({ hello: 'w0rld' })
-}
+})
+
+
 
 // Function for Run the server!
 fastify.listen({ port: 3001 }, (err) => {
   if(err){
-    fatify.log.error(err)
+    fastify.log.error(err)
     process.exit(1)
   }
 })
